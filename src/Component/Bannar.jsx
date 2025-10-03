17b89b0fe384b79ff777cdd679242d11bc701741
jakiadp <jakiadp@gmail.com>
2025-10-04 03:42:07 +0600

    add to bannar filr

--- src/Component/Bannar.jsx
+++ src/Component/Bannar.jsx
@@ -1,17 +1,56 @@
+import { motion } from "motion/react"
 import React from 'react';
+
+import bannarImage1 from '../assets/Bannar/bannat-image1.jpg';
+
+import bannarImage2 from '../assets/Bannar/bannat-image2.jpg';
 
 const Bannar = () => {
     return (
         <div>
             <div className="hero bg-base-100 min-h-screen">
   <div className="hero-content flex flex-col lg:flex-row-reverse justify-around gap-20">
-    <img
-      src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
-      className="max-w-sm rounded-lg shadow-2xl"
-    />
+    
+<div className="flex-1">
+        <motion.img
+        src={bannarImage1}
+        alt="Image 1"
+        
+        animate={{ y: [100, 150, 100] , opacity:2  }}
+        transition={{ duration: 5, repeat:Infinity}}
+        className=" max-w-sm shadow-2xl rounded-t-[40px] rounded-br-[40px] border-blue-500 border-s-8 border-b-8 "
+      />
+
+      {/* Image 2 */}
+ <motion.img
+        src={bannarImage2}
+        alt="Image 1"
+        
+        animate={{ x: [100, 170, 100] , opacity:2  }}
+        transition={{ duration: 10, delay:5, repeat:Infinity}}
+        className=" max-w-sm shadow-2xl rounded-t-[40px] rounded-br-[40px] border-blue-500 border-s-8 border-b-8 "
+      />
+</div>
+
+
     <div>
-      <h1 className="text-5xl font-bold">The Easiest Way <br />
-to Get Your New Job</h1>
+
+
+
+       <h1 
+      
+       className="text-5xl font-bold">The Easiest Way 
+           </h1>
+        
+        <motion.h1 
+       initial={{ scale: 0 }} animate={{ scale: 1 ,
+        transition: { duration: 2 }
+        }}
+          
+
+       className="text-5xl font-bold text-blue-700">
+       to Get Your New Job</motion.h1>
+
       <p className="py-6">
        Each month, more than 3 million job seekers turn to <br />
 website in their search for work, making over 140,000 <br />
--- src/Layout/Root.jsx
+++ src/Layout/Root.jsx
@@ -5,7 +5,7 @@ import Footer from '../Pages/Share/Footer';
 
 const Root = () => {
     return (
-        <div className='w-11/12 mx-auto'>
+        <div className='max-w-7xl mx-auto'>
             <Navber></Navber>
             <Outlet></Outlet>
             <Footer></Footer>
